8326e8d2eec84b70c11868499640702ed37c46a8
bhawatech <bhawa.inceptions@gmail.com>
2019-05-31 12:50:22 +0545

    My assignment pushed in new directry after complition. hence, the project folder will be imported in provided gir repository.

---
+++ app/src/main/java/com/example/assignmentfour/Items.java
@@ -0,0 +1,43 @@
+package com.example.assignmentfour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class Items extends AppCompatActivity {
+ImageView imageShow;
+TextView name,price,desc;
+String title;
+Bundle bundle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items);
+        bundle=getIntent().getExtras();
+    Init();
+    }
+
+    private void Init(){
+
+        imageShow=findViewById(R.id.imagename);
+        name=findViewById(R.id.nameitem);
+        price=findViewById(R.id.priceitem);
+        desc=findViewById(R.id.descitem);
+
+        if (bundle!=null){
+
+            name.setText(bundle.getString("name"));
+
+            price.setText(bundle.getString("price"));
+            desc.setText(bundle.getString("desc"));
+            title=bundle.getString("name");
+            setTitle("You are viewing:"+title );
+           String image= bundle.getString("image");
+
+           Picasso.with(this).load(image).into(imageShow);
+        }
+    }
+}
